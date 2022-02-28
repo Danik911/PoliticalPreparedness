adbc77b786f3c50a9b932c29e1e4729f5d8eb9cb
Daniil <porvipop@mail.ru>
2022-02-28 18:23:07 +0300

    Setup network

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -6,7 +6,7 @@ class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = ""
 
         fun getClient(): OkHttpClient {
             return Builder()
