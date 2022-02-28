8175dcc641089f5ee839ec9d38313bf1d0ce937a
Daniil <porvipop@mail.ru>
2022-02-28 20:44:32 +0300

    Create VoteInfoFragment, ViewModel, layout

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoFragment.kt
@@ -1,8 +1,13 @@
 package com.example.android.politicalpreparedness.election
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.databinding.FragmentVoterInfoBinding
+
 
 class VoterInfoFragment : Fragment() {
 
@@ -10,23 +15,31 @@ class VoterInfoFragment : Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        //TODO: Add ViewModel values and create ViewModel
+        val binding = FragmentVoterInfoBinding.inflate(inflater)
+        binding.lifecycleOwner = this
 
-        //TODO: Add binding values
+        val electionID = VoterInfoFragmentArgs.fromBundle(arguments!!).argElectionId
+        val electionDivision = VoterInfoFragmentArgs.fromBundle(arguments!!).argDivision
+        val viewModelFactory = VoterInfoViewModelFactory(requireActivity().application ,electionID ,electionDivision)
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(VoterInfoViewModel::class.java)
+        binding.viewModel = viewModel
 
-        //TODO: Populate voter info -- hide views without provided data.
-        /**
-        Hint: You will need to ensure proper data is provided from previous fragment.
-        */
+        binding.stateLocations.setOnClickListener {
+            startUrl(viewModel.voterInfo.value?.state?.get(0)?.electionAdministrationBody?.votingLocationFinderUrl)
+        }
+        binding.stateBallot.setOnClickListener {
+            startUrl(viewModel.voterInfo.value?.state?.get(0)?.electionAdministrationBody?.ballotInfoUrl)
+        }
 
-
-        //TODO: Handle loading of URLs
-
-        //TODO: Handle save button UI state
-        //TODO: cont'd Handle save button clicks
-
+        binding.saveElectionBtn.setOnClickListener {
+            viewModel.handleState()
+        }
+        return binding.root
     }
-
-    //TODO: Create method to load URL intents
+    private fun startUrl(url :String?){
+        var i  = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+    }
 
 }
--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,21 +1,95 @@
 package com.example.android.politicalpreparedness.election
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.database.ElectionDatabase
+import com.example.android.politicalpreparedness.network.models.Division
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
+import com.example.android.politicalpreparedness.repo.ElectionRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
 
-    //TODO: Add live data to hold voter info
+class VoterInfoViewModel(app: Application, id: Int, division: Division) : AndroidViewModel(app) {
+    private val database = ElectionDatabase.getInstance(app)
+    private val repository = ElectionRepository(database)
 
-    //TODO: Add var and methods to populate voter info
+    private val _errorMessage = MutableLiveData<String>("")
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
 
-    //TODO: Add var and methods to support loading URLs
+    private var _doesFollow = MutableLiveData<Boolean>()
+    val doesFollow: LiveData<Boolean>
+        get() = _doesFollow
 
-    //TODO: Add var and methods to save and remove elections to local database
-    //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
+    private val _status = MutableLiveData<ElectionListStatus>()
+    val status: LiveData<ElectionListStatus>
+        get() = _status
 
-    /**
-     * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
-     */
+
+    init {
+        viewModelScope.launch {
+            _doesFollow.value = repository.doesExist(id)
+
+        }
+    }
+
+    private val addressValue: String = checkStateAddressIfNull(division)
+    val voterInfo = liveData<VoterInfoResponse>(Dispatchers.IO) {
+        try {
+            _status.postValue(ElectionListStatus.LOADING)
+            val response = repository.getVoterInfo(id, addressValue).body()
+            emit(response!!)
+            _status.postValue(ElectionListStatus.DONE)
+
+        } catch (e: Exception) {
+            _status.postValue(ElectionListStatus.ERROR)
+
+            Log.e("VoterInfoViewModel", e.message.toString())
+
+        }
+    }
+
+    private fun followElection() {
+        if (voterInfo.value == null) {
+            return
+        }
+        viewModelScope.launch {
+            repository.saveElectionInDB(voterInfo.value!!.election)
+        }
+    }
+
+    private fun unFollowElection() {
+        if (voterInfo.value == null) {
+            return
+        }
+        viewModelScope.launch {
+            repository.deleteElection(voterInfo.value!!.election.id)
+        }
+    }
+
+    fun handleState() {
+        if (_doesFollow.value!!) {
+            _doesFollow.value = false
+            unFollowElection()
+        } else {
+            _doesFollow.value = true
+            followElection()
+        }
+    }
+
+
+    private fun checkStateAddressIfNull(division: Division): String {
+        return if (division.state.isEmpty()) {
+            _errorMessage.value = "No data available Please try another election"
+            " ${division.country} - "
+        } else {
+            "${division.country} - ${division.state}"
+        }
+
+    }
+
 
 }
